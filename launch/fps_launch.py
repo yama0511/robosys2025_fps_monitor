b204503e34aac77a893c5cfecc06a3ad4c188927
yama <s24c1031tu@s.chibakoudai.jp>
2026-01-09 01:06:02 +0900

    Update package name in launch file

--- launch/fps_launch.py
+++ launch/fps_launch.py
@@ -11,12 +11,12 @@ def generate_launch_description():
     """Generate launch description."""
     return LaunchDescription([
         Node(
-            package='fps_monitor',
+            package='robosys2025_fps_monitor',
             executable='sensor_sim',
             name='sensor_sim'
         ),
         Node(
-            package='fps_monitor',
+            package='robosys2025_fps_monitor',
             executable='fps_calc',
             name='fps_calc',
             output='screen'
